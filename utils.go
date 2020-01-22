455b55ad5b6a2b5d6b6ce96824db0072815770eb
v.kuslin <v.kuslin@untill.com>
2020-01-22 15:59:32 +0300

    CreateResp doc added

--- utils.go
+++ utils.go
@@ -1,5 +1,6 @@
 package ibus
 
+// CreateResponse creates *Response with given status code and string data
 func CreateResponse(code int, message string) *Response {
 	return &Response{
 		StatusCode: code,
