7905e1abc0ec887b05bf926b33348c03328476c1
v.kuslin <v.kuslin@untill.com>
2020-01-29 11:39:33 +0300

    CreateErrorResponse added

--- utils.go
+++ utils.go
@@ -7,3 +7,12 @@ func CreateResponse(code int, message string) *Response {
 		Data:       []byte(message),
 	}
 }
+
+// CreateErrorResponse creates *Response with given status code, error message and ContentType "plain/text"
+func CreateErrorResponse(code int, err error) *Response {
+	return &Response{
+		StatusCode:  code,
+		Data:        []byte(err.Error()),
+		ContentType: "plain/text",
+	}
+}
