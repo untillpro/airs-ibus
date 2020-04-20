be74cf6b1f11da84314c514e268f2397fcb88d5a
Denis Gribanov <d.gribanov@dev.untill.com>
2020-04-20 17:30:10 +0300

    minor

--- utils_test.go
+++ utils_test.go
@@ -221,6 +221,7 @@ func TestStopOnChannelClose(t *testing.T) {
 	testStopOnChanneClose(t, BusPacketSectionObject)
 }
 
+
 func testStopOnChanneClose(t *testing.T, bpt BusPacketType) {
 	var chunksErr error
 	chunks := make(chan []byte)
