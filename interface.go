0573f0a55374d15b943b8598dc13378e949d46e9
Denis Gribanov <instance6@gmail.com>
2020-12-16 15:35:31 +0300

    SendRequest2 (chan []byte -> chan ISection)

--- interface.go
+++ interface.go
@@ -15,8 +15,20 @@ import (
 // Non-nil chunksError when chunks are closed means an error in chunks
 // `err` and `error` can be a wrapped ErrTimeoutExpired (Checked as errors.Is(err, ErrTimeoutExpired))
 // If chunks is not nil then Response must be ignored
+// @Deprecated
 var SendRequest func(ctx context.Context,
 	request *Request, timeout time.Duration) (res *Response, chunks <-chan []byte, chunksError *error, err error)
+
+// SendRequest2 used by router and app, sends a message to a given queue
+// err is not nil -> sections and secError are nil, res must be ignored
+// sections not nil ->
+//  - res must be ignored
+//  - sections bust be read to the end
+//  - non-nil secError when sections are closed means an error in sections
+// sections is nil -> res only should be used as the result. secError and err are nil
+// `err` and `*secError` can be a wrapped ErrTimeoutExpired (Checked as errors.Is(err, ErrTimeoutExpired))
+var SendRequest2 func(ctx context.Context,
+	request Request, timeout time.Duration) (res Response, sections <-chan ISection, secError *error, err error)
 
 // RequestHandler used by app
 var RequestHandler func(ctx context.Context, sender interface{}, request Request)
