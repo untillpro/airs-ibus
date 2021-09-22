fe011767043fb74c5d0b0263267a890020db0493
Denis Gribanov <d.gribanov@dev.untill.com>
2021-09-22 12:55:35 +0300

    correct `Deprecated` annotations

--- interface.go
+++ interface.go
@@ -9,13 +9,13 @@ import (
 	"time"
 )
 
+// Deprecated: use SendRequest2
 // SendRequest used by router and app, sends a message to a given queue
 // If err is not nil res and chunks are nil
 // If chunks is not nil chunks must be read to the end
 // Non-nil chunksError when chunks are closed means an error in chunks
 // `err` and `error` can be a wrapped ErrTimeoutExpired (Checked as errors.Is(err, ErrTimeoutExpired))
 // If chunks is not nil then Response must be ignored
-// @Deprecated
 var SendRequest func(ctx context.Context,
 	request *Request, timeout time.Duration) (res *Response, chunks <-chan []byte, chunksError *error, err error)
 
@@ -38,8 +38,8 @@ var RequestHandler func(ctx context.Context, sender interface{}, request Request
 // SendResponse used by app
 var SendResponse func(ctx context.Context, sender interface{}, response Response)
 
+// Deprecated: use SendParallelResponce2
 // SendParallelResponse s.e.
-// @Deprecated
 // If chunks is not nil they must be readed by implementation to the end
 // Chunks must be closed by sender
 // response is valid when chunks finishes or nil
@@ -50,12 +50,12 @@ var SendParallelResponse func(ctx context.Context, sender interface{}, chunks <-
 // Result of Close
 var SendParallelResponse2 func(ctx context.Context, sender interface{}) (rsender IResultSenderClosable)
 
+// Depecated: use MetricCntSerialRequest
 // MetricSerialRequestCnt s.e.
-// @Depecated
 var MetricSerialRequestCnt uint64
 
+// DepecatedL use MetricDurSerialRequest
 // MetricSerialRequestDurNs s.e.
-// @Depecated
 var MetricSerialRequestDurNs uint64
 
 // MetricCntSerialRequest  number of serial requests
